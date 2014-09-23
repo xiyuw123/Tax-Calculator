ce3c4edb02aad377aeb1d269068e7fd56efd4c35
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2014-09-22 20:04:35 -0400

    added module initialization code

---
+++ fileIO/__init__.py
@@ -0,0 +1,6 @@
+'''This module provides various file reading/writing functionality.
+'''
+# allow functions defined in submodules to be accessible in fileIO
+from hdf5 import *
+from json_io import *
+from pickling import *
